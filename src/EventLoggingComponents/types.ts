24839c239e3d22353bea419956b661c0dc62f266
Hun <hun@nextunicorn.kr>
2023-05-16 00:47:27 +0900

    [REFACTOR] 이벤트 로깅 타입 리팩토링

--- src/EventLoggingComponents/types.ts
+++ src/EventLoggingComponents/types.ts
@@ -1,26 +1,29 @@
 import { EventProperty } from '@/dataEvent';
 import React from 'react';
 
-type Path<T, U extends any[] = []> = U['length'] extends 4
-  ? U
-  : keyof T extends infer K
-  ? K extends keyof T
-    ? Path<T[K], [...U, K]>
+type Path<T, TPath extends string[] = []> = TPath['length'] extends 4
+  ? TPath
+  : keyof T extends infer Key extends string
+  ? Key extends keyof T
+    ? Path<T[Key], [...TPath, Key]>
     : never
   : never;
 
 export type EventPath = Path<EventProperty>;
 type EventAction = 'click' | 'view';
 
-type EventPropertyForPath<Path extends EventPath, Action extends EventAction> = Path extends infer P
-  ? P extends [infer Feature, infer Type, infer Location, infer Target]
-    ? Feature extends keyof EventProperty
-      ? Type extends keyof EventProperty[Feature]
-        ? Location extends keyof EventProperty[Feature][Type]
-          ? Target extends keyof EventProperty[Feature][Type][Location]
-            ? Action extends keyof EventProperty[Feature][Type][Location][Target]
-              ? EventProperty[Feature][Type][Location][Target][Action]
-              : never
+type EventPropertyForPath<Path extends EventPath, Action extends EventAction> = Path extends [
+  infer Feature,
+  infer Type,
+  infer Location,
+  infer Target,
+]
+  ? Feature extends keyof EventProperty
+    ? Type extends keyof EventProperty[Feature]
+      ? Location extends keyof EventProperty[Feature][Type]
+        ? Target extends keyof EventProperty[Feature][Type][Location]
+          ? Action extends keyof EventProperty[Feature][Type][Location][Target]
+            ? EventProperty[Feature][Type][Location][Target][Action]
             : never
           : never
         : never
