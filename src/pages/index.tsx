3bc506235125655222e43277a8c8c8f3e0ada74e
Hun <hun@nextunicorn.kr>
2023-05-15 17:08:14 +0900

    [STYLE] 스타일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
-    <>
+    <div style={{ padding: '32px', display: 'flex', flexDirection: 'column', gap: '28px' }}>
       <ClickEventLogging
         path={['인사이트', '메인', '인사이트_리스트', '콘텐츠_카드']}
         property={{ insightId: 'abc', category: '엔젤투자', isBookmarked: true }}
       >
-        <button onClick={() => console.log('인사이트_카드_클릭')}>
-          {`인사이트 > 인사이트_리스트 > 인사이트_카드`}
+        <button onClick={() => console.log('인사이트_콘텐츠_카드_클릭')}>
+          {`ClickEventLogging (인사이트 > 메인 > 인사이트_리스트 > 콘텐츠_카드)`}
         </button>
       </ClickEventLogging>
 
@@ -19,8 +19,8 @@ export default function Home() {
         path={['인사이트', '메인', '인사이트_리스트', '카테고리']}
         property={{ name: '엔젤투자' }}
       >
-        <button onClick={() => console.log('카테고리_클릭')}>
-          {`인사이트 > 인사이트_리스트 > 카테고리`}
+        <button onClick={() => console.log('인사이트_카테고리_클릭')}>
+          {`ClickEventLogging (인사이트 > 메인 > 인사이트_리스트 > 카테고리)`}
         </button>
       </ClickEventLogging>
 
@@ -28,12 +28,15 @@ export default function Home() {
         path={['인사이트', '메인', '카테고리_리스트', '카테고리']}
         property={{ name: '엔젤투자' }}
       >
-        <button onClick={() => console.log('카테고리_클릭')}>
-          {`인사이트 > 카테고리_리스트 > 카테고리`}
+        <button onClick={() => console.log('인사이트_카테고리_클릭')}>
+          {`ClickEventLogging (인사이트 > 메인 >카테고리_리스트 > 카테고리)`}
         </button>
       </ClickEventLogging>
 
-      <button onClick={() => setIsVisible((prev) => !prev)}>클릭하시면 열립니다.</button>
+      <button onClick={() => setIsVisible((prev) => !prev)}>
+        ViewEventLogging 테스트 (클릭하면 열림)
+      </button>
+
       {isVisible && (
         <ViewEventLogging
           path={['인사이트', '메인', '인사이트_리스트', '콘텐츠_카드']}
@@ -42,6 +45,6 @@ export default function Home() {
           <div>{`인사이트 카드 View`}</div>
         </ViewEventLogging>
       )}
-    </>
+    </div>
   );
 }
